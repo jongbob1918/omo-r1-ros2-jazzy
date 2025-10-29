6039ce6d069c7a663361f5d11fa590f767b6d6e3
jongbob1918 <jongbob1918@gmail.com>
2025-10-29 13:14:46 +0900

    [init update]

--- build/omo_r1_interfaces/CMakeFiles/omo_r1_interfaces__cpp.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for omo_r1_interfaces__cpp.
-# This may be replaced when dependencies are built.
--- build/omo_r1_navigation2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mac/dev_ws/omo_robot/src/omo_r1-ros2/omo_r1_navigation2
-# Build directory: /home/mac/dev_ws/omo_robot/build/omo_r1_navigation2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
